32a35fe7904068b219fc77ea766359eb98f2d2a7
Chanchal Jaiswal <chanchal123jaiswal@gmail.com>
2026-04-05 21:33:36 +0530

    shift4

--- src/components/home/HomeBottomText.jsx
+++ src/components/home/HomeBottomText.jsx
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom'
 
 const HomeBottomText = () => {
   return (
-    <div className="flex  justify-center items-center gap-8 text-center mb-5">
-      <Link className='text-[6vw] uppercase font-bold border-white border-4 rounded-full' to='/work'>work</Link>
-      <Link className='text-[6vw] uppercase font-bold border-white border-4 rounded-full' to='/agency'>agency</Link>
+    <div className="flex  justify-center items-center gap-20 text-center mb-5">
+      <Link className='text-[6vw] uppercase font-bold border-white border-4 rounded-full hover:text-green-500 hover:border-green-500' to='/work'>work</Link>
+      <Link className='text-[6vw] uppercase font-bold border-white border-4 rounded-full  hover:text-green-500 hover:border-green-500' to='/agency'>agency</Link>
     </div>
   )
 }
